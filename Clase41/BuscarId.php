f0eff8a30a3d88e6c3ee280fdb05d2d49c18f56c
Sebas1444 <canhetesebas@gmail.com>
2023-02-09 07:30:08 -0300

    Clase41

---
+++ Clase41/BuscarId.php
@@ -0,0 +1,27 @@
+<?php
+# Conectamos con MySQL
+require_once('../../../../php/conexion.php');
+$id = $_POST['cod_usuario'];
+$sql="select * from usuarios where cod_usuario=".$id;
+$ret=$mysqli->query($sql);
+$row=$ret->fetch_array(MYSQLI_ASSOC);
+$objeto = new stdClass();
+$objeto->mensaje = "Registro encontrado";
+$objeto->nombre_usuario = $row["nombre_usuario"];
+$objeto->pass_usuario = $row["pass_usuario"];
+$objeto->login_usuario = $row["login_usuario"];
+$json = json_encode($objeto);
+echo($json);
+?>
+Eliminar.php
+<?php
+require_once('../../../../php/conexion.php');
+$id = $_POST['cod_usuario'];
+$sql="delete from usuarios where cod_usuario=".$id;
+$ret=$mysqli->query($sql);
+$res="Registro NO eliminado";
+if($ret==1){
+$res="Registro eliminado";
+}
+echo($res);
+?>
